450e8b845970f1ae4b13a44305a0ff2a758b176b
andriy.belash <belasha6@gmail.com>
2024-08-13 22:41:51 +0300

    add login validate

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ const routes = [
       {
         path: '/home',
         name: 'dashboard-home',
-        component: () => import('@/views/dashboard/Home.vue'),
+        component: () => import('@/views/dashboard/HomePage.vue'),
         meta: {
           layout: LayoutDefault
         }
